cd6e9388fd9cbacfe5b907e979984dd6304f3ab1
Stephen Liu <steuyve@gmail.com>
2022-06-25 14:11:06 +0800

    Update html files.

--- mathjax-config.js
+++ mathjax-config.js
@@ -1,5 +1,7 @@
 window.MathJax = {
+    loader: {load: ['[tex]/mathtools']},
     tex: {
+        packages: {'[+]': ['mathtools']},
         inlineMath: [['$', '$'], ['\\(', '\\)']],
         tags: 'ams'
     }
